5b98b8c06a9c359a79a0feb70f26b24b2ea99724
mugdhoreo <mugdhoreo@gmail.com>
2026-09-21 11:45:55 +0600

    feat: integrate patient routes into server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
+app.use('/api/patients', require('./routes/patientRoutes'));
 
 app.get('/', (req, res) => {
   res.send('Clinic Management System API');
